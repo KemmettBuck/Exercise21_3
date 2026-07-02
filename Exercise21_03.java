417e71222c890a7532b4d83540d8b65895726e2d
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-01 21:58:57 -0400

    display, count and display keywords, insert java keywords, use Set from java.util

--- Exercise21_03.java
+++ Exercise21_03.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Exercise21_03 {
     public static void main(String[] args) throws Exception {
@@ -14,6 +17,36 @@ public class Exercise21_03 {
             System.out.println("File " + args[0] + " does not exist");
             System.exit(2);
         }
+
+        // display
+        System.out.println("The program is");
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine()) {
+            System.out.println(input.nextLine());
+        }
+        input.close();
+
+        // count and display keywords 
+        System.out.println("The number of keywords in the program is " 
+        + countKeywords(file));
+    }
+    public static int countKeywords(File file) throws Exception {
+
+        // keywords
+        String[] keywordString = {"abstract", "assert", "boolean",
+            "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum",
+            "extends", "for", "final", "finally", "float", "goto",
+            "if", "implements", "import", "instanceof", "int",
+            "interface", "long", "native", "new", "package", "private",
+            "protected", "public", "return", "short", "static",
+            "strictfp", "super", "switch", "synchronized", "this",
+            "throw", "throws", "transient", "try", "void", "volatile",
+            "while", "true", "false", "null"};
+
+        Set<String> keywordSet =
+            new HashSet<>(Arrays.asList(keywordString));
+        int count = 0;
     }
     
 }
