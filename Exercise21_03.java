8ce921729e45fdf4201f72e439c7aa85c5ec5aba
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-01 21:52:05 -0400

    fix check filename, add check file status if exists

--- Exercise21_03.java
+++ Exercise21_03.java
@@ -3,6 +3,17 @@ public class Exercise21_03 {
     public static void main(String[] args) throws Exception {
         
         // check if filename provided
+        if (args.length != 1) {
+            System.out.println("Error: Provide a source file.");
+            System.exit(1);
+        }
+
+        //check file status
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.out.println("File " + args[0] + " does not exist");
+            System.exit(2);
+        }
     }
     
 }
